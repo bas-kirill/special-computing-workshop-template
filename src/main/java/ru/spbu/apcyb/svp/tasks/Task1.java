72ca6ab3636116a576a80459411f1b5c96fb91c4
eertree <baskirill.an@gmail.com>
2023-02-23 12:16:49 +0200

    feat task 1: atm machine

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
@@ -1,11 +1,108 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import lombok.extern.java.Log;
+
+class Task1Util {
+
+    /**
+     * Перевести строку в сумму.
+     */
+    public static long strToSum(String str) {
+        long result;
+        try {
+            result = Long.parseLong(str);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ошибка - не целое число!");
+        }
+        if (result > 0) {
+            return result;
+        } else {
+            throw new ArithmeticException("Ошибка - не натуральное число!");
+        }
+    }
+
+    /**
+     * Преобразование строки в массив.
+     */
+    public static Long[] strToLongArray(String str) {
+        List<Long> notes = new ArrayList<>();
+        String[] substrings = str.split(" ");
+        for (String tmp : substrings) {
+            notes.add(strToSum(tmp));
+        }
+        Set<Long> uniq = new TreeSet<>(notes);
+        notes.clear();
+        notes.addAll(uniq);
+        notes.sort(Collections.reverseOrder());
+        return notes.toArray(new Long[0]);
+    }
+}
+
 /**
- * Задание 1.
+ * Задание 1. Банкомат.
  */
+@Log
 public class Task1 {
 
-  public static void main(String[] args) {
+    public static final Scanner IN = new Scanner(System.in);
 
-  }
+    /**
+     * Поиск всех комбинаций.
+     */
+    public static int combinations(Long[] banknotes, long sum, long min, String str) {
+        int cnt = 0;
+        if (sum == 0) {
+            log.info(str);
+            cnt++;
+            return cnt;
+        }
+
+        List<Long> newNotes = new ArrayList<>(Arrays.asList(banknotes));
+        for (long banknote : banknotes) {
+            if (sum - banknote < banknote) {
+                newNotes.remove(0);
+            }
+            if (min >= banknote && sum >= banknote) {
+                Long[] tmp = newNotes.toArray(new Long[0]);
+                cnt += combinations(tmp, sum - banknote, banknote, str + banknote + " ");
+            }
+        }
+        return cnt;
+    }
+
+    /**
+     * Разложение строки на массив номиналов.
+     */
+    public static Long[] readBanknotes() {
+        log.info("Введите номиналы через пробел: ");
+        String str = IN.nextLine();
+        return Task1Util.strToLongArray(str);
+    }
+
+    /**
+     * Ввод суммы.
+     */
+    public static long inputSum() {
+        log.info("Введите сумму: ");
+        String str = IN.nextLine();
+        return Task1Util.strToSum(str);
+    }
+
+    /**
+     * Входная точка в программу.
+     */
+    public static void main(String[] args) {
+        long sum = inputSum();
+        Long[] notes = readBanknotes();
+        log.info("Комбинации:");
+        int cnt = combinations(notes, sum, notes[0], "");
+        log.info(() -> "Количество комбинация: " + cnt);
+    }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
@@ -1,8 +1,77 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * Тесты для задания 1.
  */
-public class Task1Test {
+class Task1Test {
 
+    @Test
+    void strToLongArray_ints() {
+        Long[] exp = new Long[] {34L, 2L, 1L};
+        assertEquals(Arrays.asList(exp), Arrays.asList(Task1Util.strToLongArray("1 34 2")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 34 -2", "2 a 3", "", "1 34,1 2"})
+    void strToLongArray_error(String str) {
+        assertThrows(
+                Exception.class,
+                () -> Task1Util.strToLongArray(str)
+        );
+    }
+
+    @Test
+    void variants_right() {
+        Long[] notes = new Long[] {100L, 50L, 20L};
+        assertEquals(6, Task1.combinations(notes, 200, notes[0], ""));
+    }
+
+    @Test
+    void variants_noChange() {
+        Long[] notes = new Long[] {100L};
+        assertEquals(0, Task1.combinations(notes, 110, notes[0], ""));
+    }
+
+    @Test
+    void variants_test1() {
+        Long[] notes = new Long[] {3L, 2L};
+        assertEquals(1, Task1.combinations(notes, 5, notes[0], ""));
+    }
+
+    @Test
+    void variants_test2() {
+        Long[] notes = new Long[] {2L, 1L};
+        assertEquals(3, Task1.combinations(notes, 4, notes[0], ""));
+    }
+
+    @Test
+    void strToLong_int() {
+        assertEquals(15, Task1Util.strToSum("15"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "some txt", "1,5"})
+    void strToSum_illegal_argument(String str) {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> Task1Util.strToSum(str)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-300"})
+    void strToSum_incorrect_value(String str) {
+        assertThrows(
+                ArithmeticException.class,
+                () -> Task1Util.strToSum(str)
+        );
+    }
 }
