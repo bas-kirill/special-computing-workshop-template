e0f3aae6f574f26d138e729843cde68fd8a57621
eertree <baskirill.an@gmail.com>
2023-02-23 13:39:51 +0200

    feat task 3: rec walk dirs

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,56 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
+
 /**
  * Задание 3.
  */
 public class Task3 {
+    protected void printFiles(File curRoot, FileWriter fileWriter) throws IOException {
+        for (File curFile : Objects.requireNonNull(curRoot.listFiles())) {
+            if (curFile.isDirectory()) {
+                printFiles(curFile, fileWriter);
+            } else {
+                fileWriter.write(curFile.getPath().concat("\n"));
+            }
+        }
+    }
 
-  public static void main(String[] args) {
+    private void mainLogic(String root, String output) throws IOException {
+        File rootDir = new File(root);
+        if (!rootDir.exists() || !rootDir.isDirectory()) {
+            throw new IOException("directory doesn't exist ");
+        }
+        if (!rootDir.isDirectory()) {
+            throw new IOException("input path isn't a directory");
+        }
+        File outputFile = new File(output);
 
-  }
+        if (outputFile.isDirectory()) {
+            throw new IOException("output file can't be a directory");
+        }
+        try (FileWriter fileWriter = new FileWriter(outputFile)) {
+            this.printFiles(rootDir, fileWriter);
+        } catch (IOException e) {
+            throw new IOException("cant write into file");
+        }
+    }
+
+    /**
+     * Программа выводит содержимое директории в отдельный файл.
+     *
+     * @param args - путь до директории, содержимое которой необходимо вывести и путь до файла,
+     * в который осуществляется вывод.
+     * @throws IOException ошибка в задании пути до дериктории.
+     */
+    public static void main(String[] args) throws IOException {
+        Task3 task3 = new Task3();
+        if (args.length != 2) {
+            throw new IOException("need 2 parameters in command line");
+        }
+        task3.mainLogic(args[0], args[1]);
+    }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,69 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
 
+    @Test
+    @DisplayName("outFileErrorTest")
+    void outFileErrorTest() {
+        String rootTest = "src/test";
+        Task3 testclass = new Task3();
+        Assertions.assertThrows(IOException.class,
+                () -> testclass.main(new String[] {rootTest, "src"}));
+
+
+    }
+
+    @Test
+    @DisplayName("mainTest")
+    void mainTest() throws IOException {
+        String rootTest = "src/test";
+        String outputTest = "output.txt";
+        List<String> actual = new ArrayList<>();
+
+        List<String> expected = new ArrayList<>(Arrays.asList(
+                "src/test/java/ru/spbu/apcyb/svp/tasks/ArrayUtilsTest.java",
+                "src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java",
+                "src/test/java/ru/spbu/apcyb/svp/tasks/Task2Test.java",
+                "src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java",
+                "src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java",
+                "src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java",
+                "src/test/resources/.stub"));
+
+        Task3.main(new String[] {rootTest, outputTest});
+
+        try (Scanner scanner = new Scanner(new File(outputTest))) {
+            while (scanner.hasNextLine()) {
+                actual.add(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            throw new IOException("test error");
+        }
+
+        expected.sort(null);
+        actual.sort(null);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("dirErrorTest")
+    void dirErrorTest() {
+        String rootTest = "src/error";
+        Task3 testclass = new Task3();
+        Assertions.assertThrows(IOException.class,
+                () -> Task3.main(new String[] {rootTest, "output.txt"}));
+
+    }
 }
