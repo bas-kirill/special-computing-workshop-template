f639ede41c5f1f6cd4911c5a3563749674711a73
eertree <baskirill.an@gmail.com>
2023-02-23 13:49:29 +0200

    feat task 4: concurrency file processing

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
@@ -1,8 +1,36 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.FileNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * Тесты для задания 4.
  */
-public class Task4Test {
+class Task4Test {
 
+    @Test
+    void inputFileNotExists() {
+        assertThrows(
+                FileNotFoundException.class,
+                () -> Task4.multiThreadTan("afjafja1fanfa", "testfile", 1, 1)
+        );
+    }
+
+    @Test
+    void fileInInexistingFolder() {
+        assertThrows(
+                FileNotFoundException.class,
+                () -> Task4.multiThreadTan("numbers", "/a/b/c/d/e/f", 1, 1)
+        );
+    }
+
+    @Test
+    void normalRun() {
+        assertDoesNotThrow(
+                () -> Task4.multiThreadTan("numbers", "testfile", 10000, 10)
+        );
+    }
 }
